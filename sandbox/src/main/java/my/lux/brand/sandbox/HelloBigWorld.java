0bb01b0e6b7cb56659d6402110a193553e075da4
Alexander Fisher <alex.fisher777@live.com>
2016-02-22 22:04:49 +0200

    Change code style settings

--- sandbox/src/main/java/my/lux/brand/sandbox/HelloBigWorld.java
+++ sandbox/src/main/java/my/lux/brand/sandbox/HelloBigWorld.java
@@ -1,8 +1,8 @@
 package my.lux.brand.sandbox;
 
 public class HelloBigWorld {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello, world!!!!");
-	}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!!!!");
+    }
 }
