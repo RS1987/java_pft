2e96ee471155bfa4e19e11cefe60d964fc6e2058
Alexander Fisher <alex.fisher777@live.com>
2016-02-24 23:01:56 +0200

    Added a couple new function

--- sandbox/src/main/java/my/lux/brand/sandbox/HelloBigWorld.java
+++ sandbox/src/main/java/my/lux/brand/sandbox/HelloBigWorld.java
@@ -3,6 +3,25 @@ package my.lux.brand.sandbox;
 public class HelloBigWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!!!!");
+        hello("Alex");
+
+        double len = 5;
+        double a = 9;
+        double b = 3;
+
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!!!!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
