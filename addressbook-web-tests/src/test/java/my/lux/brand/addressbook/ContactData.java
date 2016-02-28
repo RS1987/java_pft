1c460fbc6f9332c7f9221c978e53c787e997bd4f
Alexander Fisher <alex.fisher777@live.com>
2016-02-28 17:12:09 +0200

    add new test for contacts creation

---
+++ addressbook-web-tests/src/test/java/my/lux/brand/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package my.lux.brand.addressbook;
+
+public class ContactData {
+   private final String firstname;
+   private final String middlename;
+   private final String lastname;
+   private final String nickname;
+   private final String address;
+   private final String company;
+   private final String homephone;
+
+   public ContactData(String firstname, String middlename, String lastname, String nickname, String address, String company, String homephone) {
+      this.firstname = firstname;
+      this.middlename = middlename;
+      this.lastname = lastname;
+      this.nickname = nickname;
+      this.address = address;
+      this.company = company;
+      this.homephone = homephone;
+   }
+
+   public String getFirstname() {
+      return firstname;
+   }
+
+   public String getMiddlename() {
+      return middlename;
+   }
+
+   public String getLastname() {
+      return lastname;
+   }
+
+   public String getNickname() {
+      return nickname;
+   }
+
+   public String getAddress() {
+      return address;
+   }
+
+   public String getCompany() {
+      return company;
+   }
+
+   public String getHomephone() {
+      return homephone;
+   }
+}
