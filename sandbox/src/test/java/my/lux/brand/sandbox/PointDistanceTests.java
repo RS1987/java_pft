96c0476d312ab0f23378e86defb88a3dd81a4629
Alexander Fisher <alex.fisher777@live.com>
2016-02-28 15:22:23 +0200

    Add new test for calculating distance between two points

---
+++ sandbox/src/test/java/my/lux/brand/sandbox/PointDistanceTests.java
@@ -0,0 +1,16 @@
+package my.lux.brand.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Alex on 2/28/2016.
+ */
+public class PointDistanceTests {
+
+   @Test
+   public void testDistance(){
+      Point d = new Point(4.457,6.324,25.784,45.097);
+      Assert.assertEquals(d.distance(), 44.251400633200305);
+   }
+}
