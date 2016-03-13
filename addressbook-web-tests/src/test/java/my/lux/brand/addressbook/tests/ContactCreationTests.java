558e881e08db63dfebd3779706b5161a934c06ab
Alexander Fisher <alex.fisher777@live.com>
2016-03-13 20:20:02 +0200

    Modify test creation items by adding compare two collection

--- addressbook-web-tests/src/test/java/my/lux/brand/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/my/lux/brand/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import my.lux.brand.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,13 +21,7 @@ public class ContactCreationTests extends TestBase {
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (ContactData g : after){
-            if(g.getId() > max){
-                max = g.getId();
-            }
-        }
-        contact.setId(max);
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
