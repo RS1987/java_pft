a19aac622bc8d02246d8897cdd4a2f36c65cfe17
Alexander Fisher <alex.fisher777@live.com>
2016-03-13 17:31:35 +0200

    Add contact count checking

--- addressbook-web-tests/src/test/java/my/lux/brand/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/my/lux/brand/addressbook/tests/ContactDeletionTests.java
@@ -1,6 +1,7 @@
 package my.lux.brand.addressbook.tests;
 
 import my.lux.brand.addressbook.model.ContactData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -15,9 +16,12 @@ public class ContactDeletionTests extends TestBase{
       if (! app.getContactHelper().isThereAContact()) {
          app.getContactHelper().createContact(new ContactData("A", "J", "Fisher", "Scout", "P avenue 134/98", "MorningStar", "+380887776566", "newGroup"));
       }
+      int before = app.getContactHelper().getContactCount();
       app.getContactHelper().selectContact();
       app.getContactHelper().deleteSelectedContact();
       app.getContactHelper().returnToHomePage();
+      int after = app.getContactHelper().getContactCount();
+      Assert.assertEquals(after, before - 1);
    }
 
 }
--- addressbook-web-tests/src/test/java/my/lux/brand/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/my/lux/brand/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package my.lux.brand.addressbook.tests;
 
 import my.lux.brand.addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -15,11 +16,14 @@ public class GroupModificationTests extends TestBase {
       if (! app.getGroupHelper().isThereAGroup()) {
          app.getGroupHelper().createGroup(new GroupData("test1", null, null));
       }
+      int before = app.getGroupHelper().getGroupCount();
       app.getGroupHelper().selectGroup();
       app.getGroupHelper().initGroupModification();
       app.getGroupHelper().fillGroupForm(new GroupData("newGroup", "newHeader", "newFooter"));
       app.getGroupHelper().submitGroupModification();
       app.getNavigationHelper().gotoGroupPage();
+      int after = app.getGroupHelper().getGroupCount();
+      Assert.assertEquals(after, before);
 
    }
 }
