ead0735a8275c8defd5c94a192e34f8c8208ef30
Alexander Fisher <alex.fisher777@live.com>
2016-04-10 16:27:36 +0300

    Added new modules for Mantis tests

---
+++ mantis-tests/src/test/java/my/lux/brand/mantis/tests/TestBase.java
@@ -0,0 +1,26 @@
+package my.lux.brand.mantis.tests;
+
+import my.lux.brand.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Created by Alex on 2/29/2016.
+ */
+public class TestBase {
+
+   protected static final ApplicationManager app
+           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+   @BeforeSuite
+   public void setUp() throws Exception {
+      app.init();
+   }
+
+   @AfterSuite(alwaysRun = true)
+   public void tearDown() {
+      app.stop();
+   }
+
+}
